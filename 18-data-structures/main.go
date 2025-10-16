78a7c27948e7870969a96b19072670eda792748e
felipecabeza16 <andresfelipecabeza16@gmail.com>
2025-10-16 17:57:16 -0500

    feat: strcut and data structure

--- 18-data-structures/main.go
+++ 18-data-structures/main.go
@@ -2,10 +2,34 @@ package main
 
 import "fmt"
 
+type Kage string
+
+type Ninja struct {
+	Name string
+	Age  int
+	Kage Kage
+}
+
+const (
+	Hokage     Kage = "Hokage"
+	Kazekage   Kage = "Kazekage"
+	Mizukage   Kage = "Mizukage"
+	Raikage    Kage = "Raikage"
+	Tsuchikage Kage = "Tsuchikage"
+)
+
+// tipos de datos personalizados
+func CustomTypes() {
+	Tsunade := Hokage
+	fmt.Println("Tsunade es la: ", Tsunade)
+	naruto := Ninja{
+		Name: "Naruto",
+		Age:  20,
+		Kage: Hokage,
+	}
+	fmt.Println("Naruto es la: ", naruto)
+}
+
 func main() {
-	fmt.Println("Data Structures")
-
-	var a string = "Hola, mundo!"
-	fmt.Println(a)
-
+	CustomTypes()
 }
