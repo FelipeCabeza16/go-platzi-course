a17276aded080f59e0c9b19dd5bf1f586eccf85f
felipecabeza16 <andresfelipecabeza16@gmail.com>
2025-10-16 14:16:25 -0500

    feat: class 4 variables

---
+++ variables/main.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Variables")
+
+	var a int = 1
+	var b int = 2
+	var c int = a + b
+
+	fmt.Println(c)
+
+	var cadena string = "Hola, mundo!"
+	fmt.Println(cadena)
+
+	var entero1, entero2 int = 1, 2
+	fmt.Println(entero1, entero2)
+
+	var entero3 = 3
+	fmt.Println(entero3)
+
+	var entero4 = 4
+	fmt.Println(entero4)
+
+	fruta := "manzana"
+	fmt.Println(fruta)
+
+}
