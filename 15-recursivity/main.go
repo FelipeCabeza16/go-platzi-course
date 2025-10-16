979cbc52de89b38d8bcbd45d1ad6739276c5b7e8
felipecabeza16 <andresfelipecabeza16@gmail.com>
2025-10-16 17:47:28 -0500

    feat: 15 update recursivity fibonacci

--- 15-recursivity/main.go
+++ 15-recursivity/main.go
@@ -7,8 +7,17 @@ func main() {
 
 	resultado := factorial(5)
 	fmt.Println(resultado)
-}
 
+	var fibonacci func(int) int
+	fibonacci = func(n int) int {
+		if n < 2 {
+			return n
+		}
+		return fibonacci(n-1) + fibonacci(n-2)
+	}
+	fmt.Println(fibonacci(7))
+
+}
 func factorial(n int) int {
 	if n == 0 {
 		return 1
